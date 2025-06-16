7331a943ebf4664a2faad83a41bdeedfaea65258
Cristian Krone <cristian_krone@hotmail.com>
2025-06-16 19:32:58 -0300

    FEAT[FRONT] Removido códigos não utilizados.

--- src/components/header/NavBar.vue
+++ src/components/header/NavBar.vue
@@ -1,12 +1,9 @@
 <template>
     <v-container>
         <v-toolbar-title>Baroni Bebidas</v-toolbar-title>
-
         <v-spacer></v-spacer>
-
         <v-btn text to="/">Home</v-btn>
         <v-btn text to="/exportar-bonificacao">Exportar Bonificação</v-btn>
-        <v-btn text to="/produtos">Produtos</v-btn>
     </v-container>
 </template>
 <script lang="ts">
